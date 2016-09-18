2906456045902ff923cc2785273bbfd1b7e0ff6e
Sergey Malkevich <s.malkevich@gmail.com>
2016-09-18 16:35:42 +0300

    no message

---
+++ src/by/malkevich/simplecalc/Main.java
@@ -0,0 +1,91 @@
+package by.malkevich.simplecalc;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a, b ;
+		while (true) {
+			System.out.println("ВЫБЕРИТЕ ДЕЙСТВИЕ:");
+			System.out.println("add - cложение ; sub - вычитание; mult - умножение;"
+					+ " div - деление; pow - возведение в степень; q - выход ");
+			String answer = sc.nextLine();
+
+			if (answer.equals("add")) {
+				try {
+					System.out.println("СЛОЖЕНИЕ");
+					System.out.println("Введите первое число:");
+					a = sc.nextDouble();
+					System.out.println("Введите второе число:");
+					b = sc.nextDouble();
+					System.out.println("РЕЗУЛЬТАТ: " + SimpleCalc.add(a, b));
+				} catch (InputMismatchException e) {
+					System.out.println("Скорее всего вы ошиблись при вводе. Попробуйте ещё раз.");
+				}
+			}
+
+			if (answer.equals("sub")) {
+				try {
+					System.out.println("ВЫЧИТАНИЕ");
+					System.out.println("Введите первое число:");
+					a = sc.nextDouble();
+					System.out.println("Введите второе число:");
+					b = sc.nextDouble();
+					System.out.println("РЕЗУЛЬТАТ: " + SimpleCalc.sub(a, b));
+				} catch (InputMismatchException e) {
+					System.out.println("Скорее всего вы ошиблись при вводе. Попробуйте ещё раз.");
+				}
+			}
+
+			if (answer.equals("mult")) {
+				try {
+					System.out.println("УМНОЖЕНИЕ");
+					System.out.println("Введите первое число:");
+					a = sc.nextDouble();
+					System.out.println("Введите второе число:");
+					b = sc.nextDouble();
+					System.out.println("РЕЗУЛЬТАТ: " + SimpleCalc.mult(a, b));
+				} catch (InputMismatchException e) {
+					System.out.println("Скорее всего вы ошиблись при вводе. Попробуйте ещё раз.");
+				}
+			}
+
+			if (answer.equals("div")) {
+				try {
+					System.out.println("ДЕЛЕНИЕ");
+					System.out.println("Введите первое число:");
+					a = sc.nextDouble();
+					System.out.println("Введите второе число:");
+					b = sc.nextDouble();
+					System.out.println("РЕЗУЛЬТАТ: " + SimpleCalc.div(a, b));
+				} catch (InputMismatchException e) {
+					System.out.println("Скорее всего вы ошиблись при вводе. Попробуйте ещё раз.");
+				}
+
+			}
+
+			if (answer.equals("pow")) {
+				try {
+					System.out.println("ВОЗВЕДЕНИЕ В СТЕПЕНЬ");
+					System.out.println("Введите число:");
+					a = sc.nextDouble();
+					System.out.println("Введите степень:");
+					b = sc.nextDouble();
+					System.out.println("РЕЗУЛЬТАТ: " + SimpleCalc.pow(a, b));
+				} catch (InputMismatchException e) {
+					System.out.println("Скорее всего вы ошиблись при вводе. Попробуйте ещё раз.");
+				}
+			}
+			
+			if (answer.equals("q")) {
+				System.exit(0);
+			}
+
+		}
+
+	}
+
+}
---
+++ src/by/malkevich/simplecalc/SimpleCalc.java
@@ -0,0 +1,32 @@
+package by.malkevich.simplecalc;
+
+/**
+ * 
+ * @author serg
+ *
+ *         SimpleCalc class provides simple math operations.
+ *
+ */
+public final class SimpleCalc {
+
+	public static double add(double a, double b) {
+		return a + b;
+	}
+
+	public static double sub(double a, double b) {
+		return a - b;
+	}
+
+	public static double mult(double a, double b) {
+		return a * b;
+	}
+
+	public static double div(double a, double b) {
+		return a / b;
+	}
+
+	public static double pow(double a, double b) {
+		return Math.pow(a, b);
+	}
+
+}
